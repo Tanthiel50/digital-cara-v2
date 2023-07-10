7ebf475c3194e9f0cb66ba8a082d8430867cf36d
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2023-07-10 08:04:28 +0200

    nav + about page

---
+++ src/about/subheadingsData.js
@@ -0,0 +1,76 @@
+const subheadingsData = {
+  1: [
+    {
+      title: "A propos",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+  2: [
+    {
+      title: "2023 EPSI DEVOPS",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2022-2023 BELIEVEMY FULL STACK JS",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+  3: [
+    {
+      title: "2023-Alternance DEVOPS",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2020-2023 SEIF Chargée de Marketing",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+    {
+      title: "2019-2020 Jeanneau",
+      content: (
+        <>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </>
+      ),
+    },
+  ],
+};
+
+export default subheadingsData;
---
+++ src/background/Background.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/background.css";
+import video from "../assets/Ethereal_Wave_4K_Motion_Background_Loop.mp4";
+import fallBackImage from "../assets/fallback-image.png";
+
+
+const Background = () => {
+    return (
+        <>
+        <div className="shadow-overlay"></div>
+        <video 
+            playsInline 
+            autoPlay 
+            muted 
+            loop 
+            preload="auto" 
+            id="bg" 
+            poster={fallBackImage}
+        >
+            <source src={video} type="video/mp4"/>
+        </video>
+        </>
+    );
+};
+
+export default Background;
