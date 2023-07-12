f64c237bebf56f30a1679090b4e8e02b47a56e65
Tanthiel50 <66637911+Tanthiel50@users.noreply.github.com>
2023-07-12 02:14:42 +0200

    skills + projects

--- src/skills/SkillsMenu.js
+++ src/skills/SkillsMenu.js
@@ -19,20 +19,47 @@ export default class SkillsMenu extends Component {
     });
   };
 
-  renderContent(skills) {
+  renderContent = (skills) => {
     return skills.map((skill, index) =>(
-      <div key={index} 
+      <div 
+      key={index} 
       className={`skill-sub-container-${this.state.activeMenuItem}`}
       >
         <h3>{skill.title}</h3>
-        {[...Array(6)].map((_, i => (
+        {[...Array(6)].map((_, i) => (
           <div className="level-container"
             key={i} 
             className={`level-point ${i < skill.level ? "filled" : 
             "unfilled"}`}
           ></div>
-        )))}
+        ))}
+        
       </div>
     ));
   }
-}
+
+  render(){
+    const{activeMenuItem} = this.state;
+    const menuItems = ["FRONT-END","BACK-END"];
+
+    const currentIcon = activeMenuItem === 1 ? frontendIcon : backendIcon
+
+    return(
+      <div className="skill-menu">
+        {menuItems.map((item,index) => (
+          <div
+          key={index}
+          className={classNames("skill-item", {activeSkill: activeMenuItem === index + 1})}
+          onClick={() => this.handleMenuItemClick(index+1)}
+          >
+            <h2 className='skill-title'>{item}</h2>
+            </div>
+        ))}
+        <img className='skill-icon' src={currentIcon} alt='current skill'/>
+        <div className='skill-sub-container'>
+          {this.renderContent(skills[activeMenuItem])}
+        </div>
+      </div>
+    )
+  };
+};
